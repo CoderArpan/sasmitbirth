f65ecf7e47267d010e1792c46a5a0716fd4dd288
CoderArpan <arpankhan096@gmail.com>
2024-06-15 11:12:45 +0530

    "sixth change"

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <h1 className="hand-cursor">Happy Birthday!</h1>
+            <h1 className="hand-cursor">Happy Birthday! Bnatu</h1>
         </header>
     );
 }
